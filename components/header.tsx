559f6ebf2269fc8c161afa5674ac1de3524a65bb
sandazzz <rsandarisoa@gmail.com>
2024-07-25 10:08:12 +0700

    Basic Setup

---
+++ components/header.tsx
@@ -0,0 +1,5 @@
+export default function Header() {
+  return (
+    <header className="sticky top-0 z-50 w-full h-16 px-4 bg-white flex justify-center border-neutral-200 border-b "></header>
+  );
+}
